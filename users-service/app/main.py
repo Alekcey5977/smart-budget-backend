72dfd763bc942b8cd4e86d52b3e9fa0ff1deafb2
Дмитрий <dmitrijlykov89@gmail.com>
2025-11-04 21:08:43 +0700

    feat: реализовал подключение к БД и подправилм Docker файлы

--- users-service/app/main.py
+++ users-service/app/main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from app.database import create_tables, shutdown
+
+from .models import *
+from contextlib import asynccontextmanager
 import uvicorn
 
-app = FastAPI()
+
+
+@asynccontextmanager
+async def life_span(app: FastAPI):
+    await create_tables()
+    yield
+    await shutdown()
+
+
+app = FastAPI(lifespan=life_span)
 
 @app.get("/health")
 async def health():
