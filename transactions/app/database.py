ebb7b349a08f5331ee88c01de5c71d516c6be98f
Дмитрий <dmitrijlykov89@gmail.com>
2025-11-12 20:54:00 +0700

    Создание подключение к бд

---
+++ transactions/app/database.py
@@ -0,0 +1,54 @@
+import os
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.exc import SQLAlchemyError
+from dotenv import load_dotenv
+from .models import Base
+
+
+# Загружаем переменные из .env файла
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Создание асинхронного соединения для БД
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=True,
+    future=True
+)
+
+# Создание асинхронных сессий дл БД
+AsyncSessionLocal = async_sessionmaker(
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+    autoflush=False,
+    autocommit=False
+)
+
+# Асинхронное открытие сессии для эндпоинтов при взаимодействии с БД
+
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        try:
+            yield session
+            await session.commit()
+
+        except SQLAlchemyError:
+            await session.rollback()
+            raise
+
+        finally:
+            await session.close()
+
+
+# Асинхронное создание теаблиц в БД
+async def create_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+# Асинхронное закрытие соединений при остановке
+async def shutdown():
+    await engine.dispose()
