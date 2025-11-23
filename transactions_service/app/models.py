9b44b27831f76d7d2ec8558cd9164d22419c65d8
Дмитрий <dmitrijlykov89@gmail.com>
2025-11-23 16:19:29 +0700

    Добавление новых моделей в transactios_servicе (Merchant, MCC_Category), а также длавлены новые связи между таблицами и исправлены другие модели

--- transactions_service/app/models.py
+++ transactions_service/app/models.py
@@ -7,30 +7,53 @@ class Transaction_Base(DeclarativeBase):
     pass
 
 
+
 class Category(Transaction_Base):
     __tablename__ = "categories"
     
-    mcc = Column(Integer, primary_key=True, index=True)
-    group = Column(String(100), nullable=False)
+    id = Column(Integer, nullable=False, primary_key=True, index=True)
+    name = Column(String(100), nullable=False)
 
+    mcc = relationship("MCC_Category", back_populates="category")
     transactions = relationship("Transaction", back_populates="category")
 
 
 
+class MCC_Category(Transaction_Base):
+    __tablename__ = "mcc_categories"
+
+    mcc = Column(Integer, nullable=False, primary_key=True, index=True)
+    name = Column(String(100), nullable=False)
+    category_id = Column(Integer, ForeignKey("categories.id"), nullable=False)
+
+    category = relationship("Category", back_populates="mcc")
+
+
+class Merchant(Transaction_Base):
+    __tablename__ = "merchants"
+
+    id = Column(Integer, nullable=False, primary_key=True, index=True)
+    name = Column(String(200), nullable=False)
+    inn = Column(String(100), nullable=False)
+
+    transactions = relationship("Transaction", back_populates="merchant")
 
 class Transaction(Transaction_Base):
     __tablename__ = "transactions"
 
     id = Column(UUID(as_uuid=True), default=uuid.uuid4, nullable=False, primary_key=True, index=True)
     user_id = Column(Integer, nullable=False, index=True)
+    category_id = Column(Integer, ForeignKey("categories.id"), nullable=False)
     amount = Column(FLOAT, nullable=False)
-    category_mcc = Column(Integer, ForeignKey("categories.mcc"), nullable=False)
-    date_time = Column(DateTime, nullable=False)
+    created_at = Column(DateTime, nullable=False)
     type = Column(String(30), nullable=False)
+    description = Column(String(200), nullable=True)
+    merchant_id = Column(Integer, ForeignKey("merchants.id"), nullable=True)
 
+    merchant = relationship("Merchant", back_populates="transactions")
     category = relationship("Category", back_populates="transactions")
 
     def category_group(self) -> str:
         if self.category is None:
             return "Unknown"
-        return self.category.group
+        return self.category.name
