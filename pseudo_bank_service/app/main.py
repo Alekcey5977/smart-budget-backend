73aeb7a3e6bcab0a429c835c4c674e945c6cee08
Дмитрий <dmitrijlykov89@gmail.com>
2025-12-24 16:48:27 +0700

    Настройка файла main и подключение к бд

---
+++ pseudo_bank_service/app/main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.database import create_tables, shutdown
+from app.models import *
+from contextlib import asynccontextmanager
+from app.routers import pseudo_bank
+import uvicorn
+
+
+@asynccontextmanager
+async def life_span(app: FastAPI):
+    await create_tables()
+    yield
+    await shutdown()
+
+
+app = FastAPI(title="Pseudo_Bank", lifespan=life_span)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(pseudo_bank.router)
+
+
+@app.get("/health")
+async def health():
+    return {"status": "healthy", "service": "pseudo_bank"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8004)
